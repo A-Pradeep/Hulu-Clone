944597581ac060e2768cfdbd569771e28bf4ab26
Pradeep A <sharmapradeep872@gmail.com>
2021-09-23 19:18:09 +0530

    Updated Card to have Key

--- components/MoviesList.jsx
+++ components/MoviesList.jsx
@@ -3,8 +3,8 @@ import MovieCard from "./MovieCard";
 function MoviesList({ moviesList }) {
   return (
     <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-      {moviesList.map((movie) => (
-        <MovieCard result={movie} />
+      {moviesList.map((movie, index) => (
+        <MovieCard key={index} result={movie} />
       ))}
     </div>
   );
